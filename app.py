81fe319df33d465bf2107c17f3174cc1f1c0c0ae
ALLCAPs788 <capspfifer@gmail.com>
2021-03-01 20:44:27 -0500

    endpoints

--- app.py
+++ app.py
@@ -1,19 +1,21 @@
 from flask import Flask, render_template, redirect
 import pymongo
-import scrape_mars
 from flask_pymongo import PyMongo 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-
+    return render_template("index.html")
 @app.route("/recommendations")
 def book_recommend():
+    return render_template("pandas.html")
 
-
+def raw_data():
+    return render_template("raw_data.html")
     return redirect("/")
     
 
+    
 
 if __name__ == "__main__":
     app.run(debug=True)
